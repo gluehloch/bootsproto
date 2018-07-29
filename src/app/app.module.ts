0f2684c8c922c7aab23326d8c82fbf7cb4561ee1
andrewinkler <andre.winkler@web.de>
2018-07-29 21:39:03 +0200

    Under construction: Anzeige eines PDF Dokuments mit Angular 6...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule }             from '@angular/core';
-import { BrowserModule }        from '@angular/platform-browser';
-import { FormsModule }          from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
 
-import { AppComponent }         from './app.component';
+import { AppComponent } from './app.component';
+import { LebenslaufComponent } from './lebenslauf/lebenslauf.component';
 
 @NgModule({
     imports: [
@@ -20,12 +21,17 @@ import { AppComponent }         from './app.component';
             {
                 path: '',
                 component: AppComponent
+            },
+            {
+                path: 'profil_andre_winkler.pdf',
+                component: LebenslaufComponent,
             }
         ])
     ],
     declarations: [
         NavbarComponent,
-        AppComponent
+        AppComponent,
+        LebenslaufComponent
     ],
     bootstrap: [
         AppComponent
---
+++ src/app/lebenslauf/lebenslauf.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-lebenslauf',
+    templateUrl: './lebenslauf.component.html'
+})
+export class LebenslaufComponent implements OnInit {
+
+    ngOnInit() {
+    }
+
+}
