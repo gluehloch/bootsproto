a93c5420d1660157e02d8f27cdacff67ff69559d
andrewinkler <andre.winkler@web.de>
2018-09-29 20:14:24 +0200

    ADD: Jenkins pipeline script.

---
+++ pipeline.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+    /*
+    agent {
+        docker {
+            image 'node:6-alpine' 
+            args '-p 3000:3000' 
+        }
+    } 
+    */
+    stages {
+        stage('Build') { 
+            steps {
+                echo 'Start build...'
+                sh 'nodejs -v'
+                sh 'npm -v'
+                sh 'java -version'
+                sh 'javac -version'
+            }
+        }
+        stage('Test') { 
+            steps {
+                echo 'Start test...'
+                // 
+            }
+        }
+        stage('Deploy') { 
+            steps {
+                echo 'Start deploy ...'
+            }
+        }
+    }
+}
