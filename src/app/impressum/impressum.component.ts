77d4ed5cbbdda64a6773b021857cf9a5e2bdcfe9
Andre Winkler <andre.winkler@web.de>
2019-05-21 06:06:07 +0200

    FIX: Do not ready empty phantom cookies.

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -29,10 +29,15 @@ export class ImpressumComponent implements OnInit {
 
     ngOnInit() {
         this.cookies = this.findAllCookies();
-        console.dir(this.cookies);
+        if (this.cookies.length > 0) {
+            console.dir(this.cookies);
+        } else {
+            console.log('There are no cookies.');
+        }
 
         for (let i = 0; i < this.cookies.length; i++) {
-            console.dir(this.cookies[i].toJSON());
+            console.dir(this.cookies[i]);
+            // console.dir(this.cookies[i].toJSON());
         }
     }
 
@@ -50,7 +55,9 @@ export class ImpressumComponent implements OnInit {
         for (let i = 0; i < cookies.length; i++) {
             const normalizedCookie = this.cutSpaces(cookies[i]);
             const cookieKeyValue = normalizedCookie.split('=');
-            allCookies.push(new Cookie(cookieKeyValue[0], cookieKeyValue[1]));
+            if (cookieKeyValue[0]) {
+                allCookies.push(new Cookie(cookieKeyValue[0], cookieKeyValue[1]));
+            }
         }
         return allCookies;
     }
