f8ea7fd4bbca781a8e29baaa21e62c0131d8c032
Andre Winkler <gluehloch@googlemail.com>
2021-03-06 15:45:59 +0100

    Removed rxjs-compat.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgcCookieConsentService, NgcInitializeEvent, NgcStatusChangeEvent, NgcNoCookieLawEvent } from 'ngx-cookieconsent';
 // What is 'NgcCookieOptions'? 
 
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { CookieService } from './cookie/cookie.service';
 
