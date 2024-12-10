98a134116306b2d7788a46dafbbd54212649308b
gluehloch <gluehloch@googlemail.com>
2024-12-10 06:39:51 +0100

    Remove commonmark.

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { HtmlRenderer, Parser } from '@commonmark/dist/commonmark.js';
 
 import { HttpClient } from '@angular/common/http';
 
